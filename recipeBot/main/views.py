ad1285ea6c223bc4dfdfa6dce6116464d7d612bd
Anthony Ma <anthony200112@yahoo.com>
2021-09-11 10:46:47 -0600

    added templates

--- recipeBot/main/views.py
+++ recipeBot/main/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+import requests, json, os
+
 
 # Create your views here.
 def index(response):
     recipeApi = '53402d637f4345cb83aac523d64ec275'
 
-    #https://api.spoonacular.com/recipes/complexSearch
+    
+    recipeAddress = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + recipeApi
+    
+    r = requests.get(recipeAddress)
+    j = json.loads(r.text)
+
+    print(j)
+
+    return render(response, 'main/index.html', {'info':j})
