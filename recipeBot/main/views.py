d27516062d761396c32a581e666de31bc1008773
Anthony Ma <anthony200112@yahoo.com>
2021-09-11 11:37:33 -0600

    update

--- recipeBot/main/views.py
+++ recipeBot/main/views.py
@@ -4,25 +4,34 @@ import requests, json, os
 # Create your views here.
 def index(response):
     recipeApi = '53402d637f4345cb83aac523d64ec275'
+    recipeAddress = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + recipeApi
 
-    filters = '&minProtein = 30'
 
-    cuisine = '&cuisine = italian'
+    if response.method == "POST":
+        cuisine = '&cuisine=' + response.POST.get('cuisine')
+        recipeAddress = recipeAddress + cuisine
+        r = requests.get(recipeAddress)
+        j = json.loads(r.text)
+        # print(filter)
 
-    diet = '&diet = vegetarian'
+    # filters = '&minProtein = 30'
+
+    # cuisine = '&cuisine = italian'
+
+    # diet = '&diet = vegetarian'
     
-    recipeAddress = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + recipeApi + filters
+    # recipeAddress = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + recipeApi + filters
 
-    recipeTaste = 'https://api.spoonacular.com/recipes/69095/tasteWidget?apiKey=' + recipeApi
+    # recipeTaste = 'https://api.spoonacular.com/recipes/69095/tasteWidget?apiKey=' + recipeApi
 
-    recipeDesc = 'https://api.spoonacular.com/recipes/{id}/summary'
+    # recipeDesc = 'https://api.spoonacular.com/recipes/{id}/summary'
     
-    r = requests.get(recipeAddress)
-    j = json.loads(r.text)
+    # r = requests.get(recipeAddress)
+    # j = json.loads(r.text)
 
-    print(j)
+    # print(j)
 
-    return render(response, 'main/index.html', {'info':recipeTaste})
+    return render(response, 'main/index.html', {'info':j})
 
 def calculateBMImetric(request ,height:int, weight:int):
     BMI = (weight / height / height) * 10000
