b6c7f6bca160b2ad2db7562fb4076c5098be6967
Anthony Ma <anthony200112@yahoo.com>
2021-09-11 11:54:30 -0600

    update

--- recipeBot/main/views.py
+++ recipeBot/main/views.py
@@ -5,13 +5,14 @@ import requests, json, os
 def index(response):
     recipeApi = '53402d637f4345cb83aac523d64ec275'
     recipeAddress = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + recipeApi
-
-
+    result = 'test'
     if response.method == "POST":
         cuisine = '&cuisine=' + response.POST.get('cuisine')
         recipeAddress = recipeAddress + cuisine
         r = requests.get(recipeAddress)
         j = json.loads(r.text)
+
+        result = j.get('results')[0]
         # print(filter)
 
     # filters = '&minProtein = 30'
@@ -31,7 +32,7 @@ def index(response):
 
     # print(j)
 
-    return render(response, 'main/index.html', {'info':j})
+    return render(response, 'main/index.html', {'info':result})
 
 # formulas found in https://drbillsukala.com/body-mass-index-calculator/#:~:text=BMI%20imperial%20formula&text=The%20US%20imperial%20formula%20for,in%20inches%20(height%20squared).
 def calculateBmiMetric(request ,height:int, weight:int):
@@ -45,52 +46,52 @@ def calculateBmiImperial(request,feet:int, inches:int, weight:int):
     return BMI
 
 # formulas found in https://www.checkyourhealth.org/eat-healthy/cal_calculator.php
-def calculateCaloricIntakeImperial(request, gender, weight, feet, inches, age, activityLevel):
-    activityLevelClassification = {"S": "Sedentary", "LA" : "Lightly Active", "MA" : "Moderately Active", "VA": "Very Active", "EA" : "Extra Active"}
-    inchConversion = (12 * feet) + inches
-    if gender == 'M':
-        BMR = 66 + (6.3 x weight) + (12.9 x inchConversion) - (6.8 x age)
-    elif gender == 'F':
-        BMR = 655 + (4.3 x weight) + (4.7 x inchConversion) - (4.7 x age)
-    else:
-        print("Gender not specified, try again!")
+# def calculateCaloricIntakeImperial(request, gender, weight, feet, inches, age, activityLevel):
+#     activityLevelClassification = {"S": "Sedentary", "LA" : "Lightly Active", "MA" : "Moderately Active", "VA": "Very Active", "EA" : "Extra Active"}
+#     inchConversion = (12 * feet) + inches
+#     if gender == 'M':
+#         BMR = 66 + (6.3 x weight) + (12.9 x inchConversion) - (6.8 x age)
+#     elif gender == 'F':
+#         BMR = 655 + (4.3 x weight) + (4.7 x inchConversion) - (4.7 x age)
+#     else:
+#         print("Gender not specified, try again!")
   
-    if activityLevel == 'S':
-        calories = BMR * 1.2
-    elif activityLevel == 'LA':
-        calories = BMR * 1.375
-    elif activityLevel == 'MA':
-        calories = BMR * 1.55 
-    elif activityLevel == 'VA':
-        calories = BMR * 1.725
-    elif activityLevel == 'EA':
-        calories = BMR * 1.9
-    else:
-        print("activity level not specified, try again!")
+#     if activityLevel == 'S':
+#         calories = BMR * 1.2
+#     elif activityLevel == 'LA':
+#         calories = BMR * 1.375
+#     elif activityLevel == 'MA':
+#         calories = BMR * 1.55 
+#     elif activityLevel == 'VA':
+#         calories = BMR * 1.725
+#     elif activityLevel == 'EA':
+#         calories = BMR * 1.9
+#     else:
+#         print("activity level not specified, try again!")
 
-    return calories
+#     return calories
 
 #formula found in https://www.verywellfit.com/how-many-calories-do-i-need-each-day-2506873
-def calculateCaloricIntakeMetric(request, gender, weight, height, age, activityLevel):
-    activityLevelClassification = {"S": "Sedentary", "LA" : "Lightly Active", "MA" : "Moderately Active", "VA": "Very Active", "EA" : "Extra Active"}
-    if gender == 'M':
-        BMR = 66.47 + (13.75 x weight) + (5.003 x height) - (6.755 x age)
-    elif gender == 'F':
-        BMR = 655.1 + (9.563 x weight) + (1.850 x height) - (4.676 x age)
-    else:
-        print("Gender not specified, try again!")
+# def calculateCaloricIntakeMetric(request, gender, weight, height, age, activityLevel):
+#     activityLevelClassification = {"S": "Sedentary", "LA" : "Lightly Active", "MA" : "Moderately Active", "VA": "Very Active", "EA" : "Extra Active"}
+#     if gender == 'M':
+#         BMR = 66.47 + (13.75 x weight) + (5.003 x height) - (6.755 x age)
+#     elif gender == 'F':
+#         BMR = 655.1 + (9.563 x weight) + (1.850 x height) - (4.676 x age)
+#     else:
+#         print("Gender not specified, try again!")
   
-    if activityLevel == 'S':
-        calories = BMR * 1.2
-    elif activityLevel == 'LA':
-        calories = BMR * 1.375
-    elif activityLevel == 'MA':
-        calories = BMR * 1.55 
-    elif activityLevel == 'VA':
-        calories = BMR * 1.725
-    elif activityLevel == 'EA':
-        calories = BMR * 1.9
-    else:
-        print("activity level not specified, try again!")
+#     if activityLevel == 'S':
+#         calories = BMR * 1.2
+#     elif activityLevel == 'LA':
+#         calories = BMR * 1.375
+#     elif activityLevel == 'MA':
+#         calories = BMR * 1.55 
+#     elif activityLevel == 'VA':
+#         calories = BMR * 1.725
+#     elif activityLevel == 'EA':
+#         calories = BMR * 1.9
+#     else:
+#         print("activity level not specified, try again!")
 
-    return calories
+#     return calories
