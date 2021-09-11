f41d9989a232fc6b87dd7d317ce28744e868370a
AhmadAbroad22 <ahmadamali77@gmail.com>
2021-09-11 10:46:58 -0600

    added BMI conversion logic in metric and imperial to views

--- recipeBot/main/views.py
+++ recipeBot/main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def calculateBMImetric(request ,height:int, weight:int):
+    BMI = (weight / height / height) * 10000
+    return BMI
+
+
+def calculateBMIimperial(request,feet:int, inches:int, weight:int):
+    inchConversion = (12 * feet) + inches
+    BMI = ((weight * 703) / (inchConversion ** 2))
+    return BMI
