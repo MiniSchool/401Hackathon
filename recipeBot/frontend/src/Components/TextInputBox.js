fe9c946f10150664c128dc1778d8056897e4830e
Philip Wang <philipwang313@gmail.com>
2021-09-12 00:35:57 -0600

    :)

--- recipeBot/frontend/src/Components/TextInputBox.js
+++ recipeBot/frontend/src/Components/TextInputBox.js
@@ -7,12 +7,20 @@ class TextInputBox extends Component {
     // }
     textRef = React.createRef();
 
+    handleEnter(e) {
+        //Enter pressed
+        if (e.key === "Enter") {
+            document.getElementById("send-button").click();
+        }
+    }
+
+
     render() {
         return (
-            <div className="input-zone">
-                <div className="input-group mb-3 input-box">
-                    <input id="text-input" type="text" className="form-control" placeholder="Chat with me" aria-label="Chat with Me" aria-describedby="button-addon2" ref={this.textRef} />
-                    <button className="btn btn-secondary" type="button" id="button-addon2" onClick={e => this.props.sendClick(this.textRef.current.value)}>Send</button>
+            <div className="reply-zone">
+                <div className="input-group mb-3 input-box zone-center">
+                    <input id="text-input" type="text" className="form-control" placeholder="Chat with me" aria-label="Chat with Me" aria-describedby="send-button" onKeyPress={this.handleEnter} ref={this.textRef} />
+                    <button className="btn btn-secondary" type="submit" id="send-button" onClick={e => this.props.sendClick(this.textRef.current.value)} >Send</button>
                 </div>
             </div>
         )
